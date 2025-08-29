2e97b5aa7a92363d9725d6805cfdad606129fd0b
Dennis Kostka <kostka@pitt.edu>
2025-08-29 14:38:47 -0400

    Automated publish for version 1.0.9

--- dist/seimodel/__init__.py
+++ dist/seimodel/__init__.py
@@ -2,6 +2,7 @@ from .src.get_seimodels import *
 from .src.sei_head import *
 from .src.sei_trunk import *
 from .src.sei_projection import *
+from .src.tml_mixin.mixin import *
 # Optionally, define __all__ for explicit exports
 __all__ = [
     'SeiTrunk', 'SeiHead', 'SeiProjection',
