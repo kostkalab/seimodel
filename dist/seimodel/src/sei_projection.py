c1fa991c716d5d6e29b98917e32ad1af376ce51e
Elizabeth Gilfeather <elizabeth.gilfeather@gmail.com>
2025-09-18 15:09:53 -0400

    Automated publish for version 1.0.23

--- dist/seimodel/src/sei_projection.py
+++ dist/seimodel/src/sei_projection.py
@@ -16,7 +16,7 @@ def read_class_annot(filepath: resources.abc.Traversable) -> list[str]:
     
 
 def sc_hnorm_varianteffect(chromatin_profile_ref, chromatin_profile_alt, histone_inds, device):
-    histone_inds = torch.tensor(histone_inds, device=device)
+    histone_inds = histone_inds.clone().to(device)
     chromatin_profile_ref_adjust = chromatin_profile_ref.clone()
     chromatin_profile_ref_adjust[:, histone_inds] = chromatin_profile_ref_adjust[:, histone_inds] * (
         (chromatin_profile_ref[:, histone_inds].sum(axis=1)*0.5 +
